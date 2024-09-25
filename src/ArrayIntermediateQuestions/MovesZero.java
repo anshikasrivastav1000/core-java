fba8878479fe4b8c5a2ebe91f4e0bc920361051a
anshikasrivastav1000 <anshikasrivastav1000@gmail.com>
2024-09-25 23:13:04 +0530

    asd

---
+++ src/ArrayIntermediateQuestions/MovesZero.java
@@ -0,0 +1,22 @@
+//package ArrayIntermediateQuestions;
+//
+//public class MovesZero {
+//    public static void main(String[] args){
+//        int n = nums.length;cx dxc
+//        int i = 0;
+//        int j = 0;
+//
+//        while(i<n){
+//            if(nums[i] !=0){
+//                nums[j] = nums[i];
+//                j++;
+//            }
+//            i++;
+//        }
+//        while(j < n){
+//            nums[j] =0;
+//            j++;
+//        }
+//    }
+//    }
+//}
