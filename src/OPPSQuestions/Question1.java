aae786efb56521100059d1c256720aa1f387b03e
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-10-03 23:22:50 +0530

    sd

---
+++ src/OPPSQuestions/Question1.java
@@ -0,0 +1,29 @@
+//package OPPSQuestions;
+//class student {
+//    String name;
+//    int age;
+//    int marks[] = new int[3];
+//    student(String name,int age, int marks[]){
+//        name = name;
+//        age = age;
+//        marks[0] =marks[0];
+//        marks[1] =marks[0];
+//        marks[2] =marks[0];
+//
+//    }
+//    public  String get_name(){
+//        return  name;
+//    }
+//    public  int get_age(){
+//        return  age;
+//    }
+//    public  int get_maximum_marks(){
+//     int maximum = marks[0];
+//     if(marks[1] >= marks[0] && marks[1] >=marks[2]){
+//         maximum = marks[1];
+//     }
+//     else if(marks[2]>=marks[0] && marks[2] >=marks[1]{  maximum = marks[2];      }
+//return maximum;
+//}
+//public class Question1 {
+//}
